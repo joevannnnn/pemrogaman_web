86ce8f3ea2469395053f14ad05ba53facec0c817
joevannnnn <140366545+joevannnnn@users.noreply.github.com>
2023-07-25 14:53:10 +0700

    aritmatika.js

---
+++ aritmatika.js
@@ -0,0 +1,13 @@
+let bil1=8;
+                   bil2=7;
+               //Operasi Aritmatika
+               let pertambahan=bil1+bil2;
+               let pengurangan=bil1-bil2;
+               let perkalian=bil1*bil2;
+               let pembagian=bil1/bil2;
+               let modulus=bil1%bil2;
+               document.write("Hasil Pertambahan "+pertambahan+"<br>");
+               document.write("Hasil Pengurangan "+pengurangan+"<br>");
+               document.write("Hasil Perkalian "+perkalian+"<br>");
+               document.write("Hasil Pembagian "+pembagian+"<br>");
+               document.write("Hasil Modulus "+modulus+"<br>");
